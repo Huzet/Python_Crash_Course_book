113cdb17cc75e0a7a6c211dd48486cb9d8e412f8
Tomas <tomas.huzevka@outlook.com>
2020-06-29 23:47:20 -0400

    added ship

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,5 +1,3 @@
-#!/bin/python3
-
 # Utilizing tutorial from Python Crash Course book written by Eric Matthes
 
 # Alien_Invasion_From_PythonCrashCourseBook
@@ -7,15 +5,25 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Overall class for the game"""
 
     def __init__(self):
         """Initializes game and resources"""
         pygame.init()
+        
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
+        self.bg_color = (self.settings.bg_color)
 
     def run_game(self):
         """Starts main loop for the game"""
@@ -24,10 +32,14 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+             # Redraw screen
+            self.screen.fill(self.bg_color)
+            self.ship.blitme()
 
-            # Make recent screen visible
-
+             # Make recent screen visible
+            pygame.display.flip()
+            
 if __name__ == '__main__':
     # Make instance run game
-    ai = AlienInvasion
-    ai.run_game
+    ai = AlienInvasion()
+    ai.run_game()
